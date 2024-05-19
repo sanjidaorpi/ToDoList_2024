3e271f34f022075b6ca85ac18dc5501602d01bde
sanjidaorpi <sanjidaorpi111@gmail.com>
2024-05-19 16:45:51 -0400

    Initial Commit

---
+++ ToDoList_2024/ToDoList_2024App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_2024App.swift
+//  ToDoList_2024
+//
+//  Created by Sanjida Orpi on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
