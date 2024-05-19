a297d073b0dc5f7ce8c0ce80cf41866e761a55c4
sanjidaorpi <sanjidaorpi111@gmail.com>
2024-05-19 17:57:18 -0400

    Complete Iteration 0 & 1.

---
+++ ToDoList_2024/NewToDoView.swift
@@ -0,0 +1,8 @@
+//
+//  NewToDoView.swift
+//  ToDoList_2024
+//
+//  Created by Sanjida Orpi on 5/19/24.
+//
+
+import Foundation
---
+++ ToDoList_2024/ToDoItem.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoItem.swift
+//  ToDoList_2024
+//
+//  Created by Sanjida Orpi on 5/19/24.
+//
+
+import Foundation
